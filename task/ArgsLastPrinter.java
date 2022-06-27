7be5d95abcf0806cc8cdae8cf7c3882522755ff1
tomohiro0123456789 <107600918+tomohiro0123456789@users.noreply.github.com>
2022-06-27 12:42:39 +0900

    課題提出

---
+++ task/ArgsLastPrinter.java
@@ -0,0 +1,11 @@
+package task;
+public class ArgsLastPrinter {
+  public static void main(String[] args) {
+      System.out.println(args.length);
+    if(args.length == 0) {
+      System.out.println("なし");
+    }else {
+      System.out.println([args.length -1]);
+    } 
+  }
+}
